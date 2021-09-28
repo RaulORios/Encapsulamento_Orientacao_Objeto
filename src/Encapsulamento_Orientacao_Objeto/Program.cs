81918be12694eda6c8acb380259dc1d0520024bb
RaulORios <raul.orios89@outlook.com>
2021-09-28 10:49:31 -0300

    Arquivos atualizados

---
+++ src/Encapsulamento_Orientacao_Objeto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento_Orientacao_Objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto("TV", 900.00, 10);
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+
+            Console.ReadKey();
+        }
+    }
+}
